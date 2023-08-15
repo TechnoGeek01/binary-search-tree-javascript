8ff46be710cd923f298e7d694a1aff250c5567a1
TechnoGeek01 <rsudharsangames@gmail.com>
2023-08-15 18:31:10 +0530

    refactored code in insert method

--- binary-search-tree.js
+++ binary-search-tree.js
@@ -16,24 +16,24 @@ class BinarySearchTree {
     if (!this.root) {
       this.root = newNode;
       return this;
-    } else {
-      var current = this.root;
-      while (true) {
-        if (value < current.value) {
-          if (!current.left) {
-            current.left = newNode;
-            return this;
-          } else {
-            current = current.left;
-          }
-        } else if (value > current.value) {
-          if (!current.right) {
-            current.right = newNode;
-            return this;
-          } else {
-            current = current.right;
-          }
+    }
+
+    var current = this.root;
+    while (true) {
+      if (value === current.value) return undefined;
+
+      if (value < current.value) {
+        if (!current.left) {
+          current.left = newNode;
+          return this;
         }
+        current = current.left;
+      } else if (value > current.value) {
+        if (!current.right) {
+          current.right = newNode;
+          return this;
+        }
+        current = current.right;
       }
     }
   }
